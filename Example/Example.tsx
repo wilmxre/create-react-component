989df153a123a6538be5203595bf2d5c326ef7a9
Kocsis Laszlo <kocsis.laszlo@kadeno.ro>
2023-08-03 00:20:10 +0300

    Create example components

---
+++ Example/Example.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Example: React.FC = () => {
+  return <></>;
+};
---
+++ ExampleWithPlaceholderProps/ExampleWithPlaceholderProps.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface ExampleWithPlaceholderPropsProps {
+}
+
+export const ExampleWithPlaceholderProps: React.FC<ExampleWithPlaceholderPropsProps> = () => {
+  return <></>;
+};
